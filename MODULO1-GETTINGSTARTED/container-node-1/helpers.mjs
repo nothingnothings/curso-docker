4f0d926a903aa21eda0b81f453db723e3b408dad
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-01-12 00:26:46 -0300

    initial commit

---
+++ MODULO1-GETTINGSTARTED/container-node-1/helpers.mjs
@@ -0,0 +1,15 @@
+const connectToDatabase = () => {
+  const dummyPromise = new Promise((resolve, reject) => {
+    setTimeout(
+      () => {
+        resolve();
+      },
+
+      1000
+    );
+  });
+
+  return dummyPromise;
+};
+
+export default connectToDatabase;
