03e1feb96b0be3a3a6eb55cd362eace6de8d7b69
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-26 02:18:27 -0300

    finished lesson 31

--- MODULO9-FAZENDO-DEPLOY-DE-DOCKER-CONTAINERS/multi-container-app-ecs/frontend/src/App.js
+++ MODULO9-FAZENDO-DEPLOY-DE-DOCKER-CONTAINERS/multi-container-app-ecs/frontend/src/App.js
@@ -4,6 +4,19 @@ import GoalInput from './components/goals/GoalInput';
 import CourseGoals from './components/goals/CourseGoals';
 import ErrorAlert from './components/UI/ErrorAlert';
 
+
+
+//  é assim que vamos DIFERENCIAR O CÓDIGO DE CONNECT DO REACT APP FRONTEND, durante development e production (PQ NÃO PODEMOS USAR AS ENVIRONMENT VARIABLES DO DOCKER COM O REACT APP, JUSTAMENTE PQ O REACT APP VAI RODAR NO BROWSER DOS USERS, E NÃO EM 1 MÁQUINA EM QUE O DOCKER ESTÁ RODANDO)...
+const backendUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:80'
+    : 'URL-EM-QUE-VAMOS-RODAR-O-BACKEND-NO-AWS-ECS'; //FORMATO: 'https://ecs-lb-121251251.us-east-2.elb.amazonaws.com'
+
+
+
+
+
+
 function App() {
   const [loadedGoals, setLoadedGoals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -26,8 +39,11 @@ function App() {
         // AO '''MESMO SERVER QUE FOI USADO PARA FAZER SERVE DESSE WEBSITE'''... (que, no caso, será o servidor node em que DEFINIMOS OS ENDPOINTS...)
 
         //   (ESSE É UM COMPORTAMENTO DEFAULT DO BROWSER...)...
+        //VERSÃO QUE FUNCIONA EM PROD, NA AWS ECS (basta que vc coloque O FRONTEND E O BACKEND EM 1 MESMA TASK, em 1 mesmo contexto... aí essa route sem o DOMAIN vai funcionar)...
+        //?  mas como NÃO  CONSEGUIMOS FAZER SERVE DO FRONTEND E DO BACKEND EM 1 MESMO SERVICE/TASK, SOMOS FORÇADOS A DEIXAR DE LADO ESSE APPROACH...
+        // const response = await fetch('/goals');
 
-        const response = await fetch('/goals');
+        const response = await fetch(backendUrl);
 
         const resData = await response.json();
 
@@ -55,7 +71,9 @@ function App() {
       //! VERSÃO QUE FUNCIONA DE MODO LOCAL... (ambiente de dev, dockerfile de development)...
       // const response = await fetch('http://localhost/goals', {
       //VERSÃO QUE FUNCIONA EM PROD, NA AWS ECS (basta que vc coloque, na ecs, O FRONTEND E O BACKEND EM 1 MESMA TASK, em 1 mesmo contexto... aí essa route sem o DOMAIN vai funcionar)...
-      const response = await fetch('/goals', {
+      //?  mas como NÃO  CONSEGUIMOS FAZER SERVE DO FRONTEND E DO BACKEND EM 1 MESMO SERVICE/TASK, SOMOS FORÇADOS A DEIXAR DE LADO ESSE APPROACH...
+      // const response = await fetch('/goals', {
+      const response = await fetch(backendUrl, {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -96,9 +114,11 @@ function App() {
     try {
       //! VERSÃO QUE FUNCIONA DE MODO LOCAL... (ambiente de dev, dockerfile de development)...
       // const response = await fetch('http://localhost/goals/' + goalId, {
-        
+
       //VERSÃO QUE FUNCIONA EM PROD, NA AWS ECS (basta que vc coloque, na ecs, O FRONTEND E O BACKEND EM 1 MESMA TASK, em 1 mesmo contexto... aí essa route sem o DOMAIN vai funcionar)...
-      const response = await fetch('/goals/' + goalId, {
+      //?  mas como NÃO  CONSEGUIMOS FAZER SERVE DO FRONTEND E DO BACKEND EM 1 MESMO SERVICE/TASK, SOMOS FORÇADOS A DEIXAR DE LADO ESSE APPROACH...
+      // const response = await fetch('/goals/' + goalId, {
+      const response = await fetch(backendUrl + '/' + goalId, {
         method: 'DELETE',
       });
 
