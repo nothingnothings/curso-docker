f695236410d0f2d0787fc0674e47639386459410
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-29 10:55:00 -0300

    finished lesson 11

--- MODULO12-KUBERNETES-EM-ACAO-VENDO-OS-CORE-CONCEPTS/primeiro-project-kubernetes/app.js
+++ MODULO12-KUBERNETES-EM-ACAO-VENDO-OS-CORE-CONCEPTS/primeiro-project-kubernetes/app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <h1>This is a second hello!</h1>
+    <p>My deployment got updated.</p>
   `);
 });
 
