b3ddbc9162b286937fe0171db3c7e6e45919e6c9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-31 16:49:03 -0300

    finished lesson 5

--- MODULO14-NETWORKING-COM-O-KUBERNETES/projeto-container-pod-node-communication/users-api/users-app.js
+++ MODULO14-NETWORKING-COM-O-KUBERNETES/projeto-container-pod-node-communication/users-api/users-app.js
@@ -23,8 +23,9 @@ app.post('/signup', async (req, res) => {
   }
 
   try {
-    // const hashedPW = await axios.get('http://auth/hashed-password/' + password); /// isso NÃO FUNCIONARÁ no mundo kubernetes, pq no MUNDO KUBERNETES NÃO EXISTEM 'DOCKER NETWORKS', e justamente por isso o 'auto name-resolve' do docker, que troca os nomes dos containers PELOS __ ACTUAL IPS INTERNOS DOS CONTAINERS, não funcionará...
-    const hashedPw = 'dummy';
+    //  const hashedPW = await axios.get('http://auth/hashed-password/' + password); /// isso NÃO FUNCIONARÁ no mundo kubernetes, pq no MUNDO KUBERNETES NÃO EXISTEM 'DOCKER NETWORKS', e justamente por isso o 'auto name-resolve' do docker, que troca os nomes dos containers PELOS __ ACTUAL IPS INTERNOS DOS CONTAINERS, não funcionará...
+     const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}` + '/hashed-password/' + password); 
+    // const hashedPw = 'dummy';
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
     res.status(201).json({ message: 'User created!' });
@@ -54,16 +55,17 @@ app.post('/login', async (req, res) => {
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
-  // const response = await axios.get(
-  //   'http://auth/token/' + hashedPassword + '/' + password
-  // );    ///// /// isso NÃO FUNCIONARÁ no mundo kubernetes, pq no MUNDO KUBERNETES NÃO EXISTEM 'DOCKER NETWORKS', e justamente por isso o 'auto name-resolve' do docker, que troca os nomes dos containers PELOS __ ACTUAL IPS INTERNOS DOS CONTAINERS, não funcionará...
+  const response = await axios.get(
+    // 'http://auth/token/' + hashedPassword + '/' + password  // sem env variables
+    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password // ?com env variables
+  );    ///// /// isso NÃO FUNCIONARÁ no mundo kubernetes, pq no MUNDO KUBERNETES NÃO EXISTEM 'DOCKER NETWORKS', e justamente por isso o 'auto name-resolve' do docker, que troca os nomes dos containers PELOS __ ACTUAL IPS INTERNOS DOS CONTAINERS, não funcionará...
 
-  const response = {
-    status: 200,
-    data: {
-      token: 'abc',
-    },
-  };
+  // const response = {
+  //   status: 200,
+  //   data: {
+  //     token: 'abc',
+  //   },
+  // };
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
   }
